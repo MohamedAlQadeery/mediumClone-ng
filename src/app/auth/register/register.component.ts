e57be7213f3a2cf4a0b7e4c64f5efb1eddde0e74
Mohamed AlQadeery <moh9amad1@gmail.com>
2023-07-08 01:25:39 +0300

    create tmp register form / installing ngx store

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,8 +1,26 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'mc-register',
   templateUrl: './register.component.html',
   standalone: true,
+  imports: [ReactiveFormsModule],
 })
-export class RegisterComponent {}
+export class RegisterComponent {
+  constructor(private _formBuilder: FormBuilder) {}
+  registerForm = this._formBuilder.nonNullable.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    streetAddress: ['', Validators.required],
+    city: ['', Validators.required],
+    state: ['', Validators.required],
+    zipCode: ['', Validators.required],
+  });
+
+  onSubmit() {
+    console.log('form', this.registerForm.getRawValue());
+  }
+}
